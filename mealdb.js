fa138f3b2d88c98a05369232baeee4ac84a254ed
jobayercse71 <jobayercse71.com>
2021-09-06 15:33:07 +0600

    nav add

--- mealdb.js
+++ mealdb.js
@@ -6,7 +6,7 @@ const searchFood = () => {
 
     }
     else {
-        // console.log(searchValue);
+       
 
         search.value = '';
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
@@ -21,32 +21,44 @@ const searchFood = () => {
 
     }
 
-
 }
+
+
+// display in UI
 
 const displayFood = (meal) => {
 
-    // console.log('this',meal);
+    
     if (meal === null) {
-        alert('not found');
+
+        const foodSection = document.getElementById('food-section');
+        foodSection.textContent = '';
+
+        document.getElementById('not-found').classList.remove('d-none');
+
     }
     else {
         const foodSection = document.getElementById('food-section');
         foodSection.textContent = '';
+        document.getElementById('not-found').classList.add('d-none');
+
 
         meal.forEach(element => {
-            //   console.log(element)
+        
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div onclick="loadDetail(${element.idMeal})" class="card">
-                <img src="${element.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${element.strMeal}</h5>
-                  <p class="card-text">${element.strInstructions.slice(0, 200)}.</p>
-                </div>
-              </div>
-        `;
+            
+            <div  onclick="loadDetail(${element.idMeal})" class="card h-100">
+             <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${element.strMeal}</h5>
+            <p class="card-text">${element.strInstructions.slice(0, 200)}</p>
+            </div>
+            <div class="card-footer d-none">
+            <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+            </div>`;
             foodSection.appendChild(div);
         });
     }
@@ -56,6 +68,7 @@ const displayFood = (meal) => {
 }
 
 
+// load detail
 const loadDetail = (id) => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
     fetch(url)
@@ -64,22 +77,70 @@ const loadDetail = (id) => {
 }
 
 
+// display detail in UI
+
 const displayDetail = (detail) => {
 
     console.log(detail);
     const mealDetails = document.getElementById('meal-details');
     mealDetails.textContent = '';
     const div = document.createElement('div');
-    div.classList.add('card');
+    div.classList.add('col');
     div.innerHTML = `
-   <img src="${detail.strMealThumb}" class="card-img-top" alt="...">
-   <div class="card-body">
+    <img src="${detail.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
      <h5 class="card-title">${detail.strMeal}</h5>
      <p class="card-text">${detail.strInstructions.slice(0, 200)}.</p>
      <a href="detail.strYoutube" class="btn btn-primary">Go somewhere</a>
-   </div>
+    </div>
    `;
     mealDetails.appendChild(div);
 
 
 }
+
+
+// auto loader 
+
+const loadFood = () => {
+
+
+
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=fish`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadDisplayFood(data.meals));
+
+}
+
+
+const loadDisplayFood = (meal) => {
+
+    const foodSection = document.getElementById('food-section');
+
+    meal.forEach(element => {
+       
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            
+        <div  onclick="loadDetail(${element.idMeal})" class="card h-100">
+         <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${element.strMeal}</h5>
+        <p class="card-text">${element.strInstructions.slice(0, 200)}</p>
+        </div>
+        <div class="card-footer d-none">
+        <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+        </div>`;
+        foodSection.appendChild(div);
+
+    });
+}
+
+// call load function for auto load
+loadFood();
+
